ddf5c14a49c422f76342738d3209a81ec3ad3c1a
Christopher Su <christophersu9@gmail.com>
2014-04-21 15:56:28 -0700

    working

--- calculate_sleep_debt.py
+++ calculate_sleep_debt.py
@@ -11,5 +11,16 @@ client = fitbit.Fitbit(secret.FITBIT_CONSUMER_KEY, secret.FITBIT_CONSUMER_SECRET
 datesToCheck = []
 for i in range(0, 7):
 	datesToCheck.append(date.today() - timedelta(days=i))
+
+totalTimeSlept = 0
 for currentDate in datesToCheck:
-	print prettyPrintTime(client.get_sleep(currentDate)['summary']['totalMinutesAsleep'])
+	timeSlept = client.get_sleep(currentDate)['summary']['totalMinutesAsleep']
+	totalTimeSlept += timeSlept
+	print unicode(currentDate) + ': ' + prettyPrintTime(timeSlept)
+print 'Total time slept: ' + prettyPrintTime(totalTimeSlept)
+
+dailyGoal = raw_input('How much do you want to sleep on average every night (in hours)? ')
+if dailyGoal:
+	totalGoalTime = int(dailyGoal)*60*len(datesToCheck)
+	print 'Total goal time: ' + prettyPrintTime(totalGoalTime)
+	print 'Difference between goal and real: ' + prettyPrintTime(totalTimeSlept-totalGoalTime)
